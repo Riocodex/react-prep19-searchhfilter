eebee783e9329072f5577661ef5e1979d2a276b7
Riocodex <onwukachibike@gmail.com>
2023-11-05 16:51:11 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searched, setSearched] = useState([]);
+  const URL = "https://jsonplaceholder.typicode.com/users"
+  const getData = async()=>{
+    const result = await fetch(URL);
+    result.json().then(json=>{
+        setUsers(json)
+    })
+  } 
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+  useEffect(() => {
+    if (searchQuery){
+      setSearched(
+        users.filter((user)=>{
+          //main code`
+          return Object.values(user).join('').toLowerCase().includes(searchQuery.toLowerCase())
+        })
+      )
+    }else{
+      setUsers(users);
+    }
+   
+  },[searchQuery])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+      type="text" 
+      className="search" 
+      placeholder='Search Users....'
+      onChange={(event)=>setSearchQuery(event.target.value)}/>
+      <h1>Users</h1>
+      <div className="grid-main">
+        {searchQuery.length > 0 ? 
+          searched.map((search)=>{
+            return(
+              <div className='grid-child'>
+                <h2>{search.name}</h2>
+                <p>{search.username}</p>
+              </div>
+            )
+          })
+         : 
+            //  {/* displays all users  */}
+           users.map((user)=>{
+            return(
+              <div className="grid-child">
+                <h2>{user.name}</h2>
+                <p>{user.username}</p>
+              </div>
+            )
+           })
+        }
+
+
+
+       
+      </div>
     </div>
   );
 }
